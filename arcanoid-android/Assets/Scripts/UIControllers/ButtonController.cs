f47006f8e7ec57819d92c77f39e311418a2ec6cb
Alex Porebski <porebski.alex@gmail.com>
2018-10-24 18:47:09 +0200

    added pauze menu

---
+++ arcanoid-android/Assets/Scripts/UIControllers/ButtonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+    public string scenToLoad;
+
+
+    public void LoadScene()
+    {
+        if (PauseMenuController.GamePaused)
+        {
+            PauseMenuController.GamePaused = false;
+            Time.timeScale = 1f;
+        }
+        SceneManager.LoadScene(scenToLoad);
+        
+    }
+
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+    
+}
---
+++ arcanoid-android/Assets/Scripts/UIControllers/PauseMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public static bool GamePaused = false;
+
+    public GameObject pauzeButton;
+
+
+
+    public  void PauseGame() {
+        GamePaused = true;
+        Time.timeScale = 0f;
+        this.gameObject.SetActive(true);
+        pauzeButton.SetActive(false);
+    }
+
+
+    public void ResumeGame() {
+        GamePaused = false;
+        Time.timeScale = 1f;
+        this.gameObject.SetActive(false);
+        pauzeButton.SetActive(true);
+    }
+
+}
