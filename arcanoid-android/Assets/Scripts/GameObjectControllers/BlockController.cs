4edb8a65f7e2f0ecbd4b80c84586e4580569ad25
Alex Porebski <porebski.alex@gmail.com>
2018-11-14 20:55:27 +0100

    explosion effects

--- arcanoid-android/Assets/Scripts/GameObjectControllers/BlockController.cs
+++ arcanoid-android/Assets/Scripts/GameObjectControllers/BlockController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class BlockController : MonoBehaviour {
 
     public int pointsValue = 50;
+    public GameObject destroyEffectPrefab;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag=="Ball") {
+            if (destroyEffectPrefab != null) {
+                GameObject destroyEffect= Instantiate(destroyEffectPrefab) as GameObject;
+                destroyEffect.transform.position = this.transform.position;
+                destroyEffect.transform.SetParent(transform.parent);
+                destroyEffect.transform.localScale = transform.localScale;
+                //destoyEffect.transform.SetParent
+                Destroy(destroyEffect, 2f);
+            }
             Destroy(gameObject);
         }
     }
