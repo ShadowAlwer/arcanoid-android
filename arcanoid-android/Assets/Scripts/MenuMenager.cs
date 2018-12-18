5d622ca2c0ff3e2114eeee4e01a12d1e5d3625af
Alex Porebski <porebski.alex@gmail.com>
2018-12-18 19:51:34 +0100

    minor improvements to game launch speed

--- arcanoid-android/Assets/Scripts/MenuMenager.cs
+++ arcanoid-android/Assets/Scripts/MenuMenager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuMenager : MonoBehaviour {
 
+    public string themeToPlay = "Theme";
 	// Use this for initialization
 	void Start () {
-        FindObjectOfType<AudioMenager>().PlayTheme("Theme");
-	}
+        FindObjectOfType<AudioMenager>().PlayTheme(themeToPlay);
+    }
 }
